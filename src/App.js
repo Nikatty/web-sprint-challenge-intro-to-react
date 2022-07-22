e8f0acbf54f2916f1a95c51ae46526e0e3185383
Nikhat Elias <nikhatelias@Nikhats-MacBook-Air.local>
2022-07-22 15:17:02 -0700

    started sprint challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import Character from "./components";
 
 const App = () => {
+
+  const [starWarsData, setStarWarsData] = useState([]);
+
+useEffect (() => {
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res =>{
+
+      (setStarWarsData(res.data))
+    
+     }) .catch(err =>
+        console.log(err))
+        return () => {
+          console.log('cleanup')
+        }
+  }, [])
+console.log(starWarsData)
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+
+function Character(props) {
+    const {} = props
+    return (
+    <h1>Hello, world</h1>
+    )
+}
+
+export default Character
